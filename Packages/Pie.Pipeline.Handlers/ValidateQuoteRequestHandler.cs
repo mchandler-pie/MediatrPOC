ee8577eacd24b91ce8cc16cff0f6b36391552161
Matt Chandler <mchandler-pie@users.noreply.github.com>
2020-02-03 16:40:42 -0700

    fixed handler

--- Packages/Pie.Pipeline.Handlers/ValidateQuoteRequestHandler.cs
+++ Packages/Pie.Pipeline.Handlers/ValidateQuoteRequestHandler.cs
@@ -9,16 +9,9 @@ namespace Packages.Pie.Pipeline.Handlers
 {
     public class ValidateQuoteRequestHandler : IRequestHandler<ValidateQuoteRequestCommand, QuoteRequestPipelineResponse>
     {
-        //private readonly IValidator _validator;
-        //public ValidateQuoteRequestHandler(IValidator validator)
-        //{
-        //    _validator = validator;
-        //}
 
         public Task<QuoteRequestPipelineResponse> Handle(ValidateQuoteRequestCommand requestCommand, CancellationToken cancellationToken)
         {
-            //_validator.Validate(requestCommand.InitialRequest);
-
             var response = new QuoteRequestPipelineResponse();
             return Task.FromResult(response);
         }
